bdd46339676321c78cd8cf50ac25b495eebe18a7
Digidon727 <ivntik77@gmail.com>
2024-03-24 18:13:35 +0600

    Наши туры

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-const BgSplide = new Splide('.splide', {
-	arrows: false,
-}).mount()
+document.addEventListener('DOMContentLoaded', function () {
+	const BgSplide = new Splide('.splide', {
+		type: 'loop',
+		perPage: 3,
+		rewind: true,
+		arrows: false,
+		gap: '20px',
+		focus: 'center',
+
+		// autoplay: true, // Включаем автоматическую прокрутку
+		// interval: 2000,
+		// pauseOnHover: true,
+	}).mount()
+})
