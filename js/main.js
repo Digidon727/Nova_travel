2b77a052ec8c536fb3a305a8d7b88b53472308d3
Digidon727 <ivntik77@gmail.com>
2024-04-06 22:24:07 +0600

    Адаптивность

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
+const menuBtn = document.querySelector('.menu__icon')
+const menu = document.querySelector('.nav__item')
+
+if (menuBtn && menu) {
+	menuBtn.addEventListener('click', () => {
+		menuBtn.classList.toggle('active')
+		menu.classList.toggle('active')
+	})
+}
+
 document.addEventListener('DOMContentLoaded', function () {
 	new Splide('#slider1', {
-		type: 'loop',
+		type: 'slide',
 		perPage: 3,
 		rewind: true,
 		arrows: false,
 		gap: '20px',
-		focus: 'center',
+		breakpoints: {
+			992: { perPage: 2 },
+			594: { perPage: 1 },
+		},
 
 		// autoplay: true, // Включаем автоматическую прокрутку
 		// interval: 2000,
@@ -18,5 +31,15 @@ document.addEventListener('DOMContentLoaded', function () {
 		arrows: false,
 		autoplay: true,
 		interval: 6000,
+		breakpoints: {
+			992: { perPage: 2 },
+			640: { perPage: 1 },
+		},
 	}).mount()
+})
+
+/*Анимация */
+AOS.init({
+	duration: 2000,
+	once: true,
 })
